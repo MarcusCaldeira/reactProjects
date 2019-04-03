b6e38bbea419a42b7965edf839b4faeac056a276
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 17:59:33 -0500

    I have to commit so everything will save so I can then find it in my terminal

--- indecisionApp/public/app.js
+++ indecisionApp/public/app.js
@@ -1,13 +1,2 @@
-console.log('App.JS is running!')
-
-
-//JSX - Javascript XML(its basically just a language extension)
-
-// const template = <p>A REACT THING</p>
-var template = React.createElement("p", null, "A REACT THING");
-const appRoot = document.getElementById('app');
-
-
-//This says we wanna render that template in that element. 
-ReactDOM.render(template, appRoot);
-
+// This is going to contain the following code, in addition to the the converted JSX(This is auto generated.)
+// This file is useless as we do not touch it 
---
+++ indecisionApp/src/app.js
@@ -0,0 +1,14 @@
+// this is going to be where the JSX is going to be stored.
+
+
+console.log('App.JS is running!')
+
+
+//JSX - Javascript XML(its basically just a language extension)
+
+const template = <p>A REACT THING</p>
+const appRoot = document.getElementById('app');
+
+
+//This says we wanna render that template in that element. 
+ReactDOM.render(template, appRoot);
