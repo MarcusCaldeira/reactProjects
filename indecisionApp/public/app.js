2bd00feaff078ee3b6ecdfe7f2306d2a6d643476
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 18:08:15 -0500

    OK WOW I finally got babel to work and yes it is auto generated.

--- indecisionApp/public/app.js
+++ indecisionApp/public/app.js
@@ -1,2 +1,18 @@
-// This is going to contain the following code, in addition to the the converted JSX(This is auto generated.)
-// This file is useless as we do not touch it 
+'use strict';
+
+// this is going to be where the JSX is going to be stored.
+
+
+console.log('App.JS is running!');
+
+//JSX - Javascript XML(its basically just a language extension)
+
+var template = React.createElement(
+  'p',
+  null,
+  'A REACT THING'
+);
+var appRoot = document.getElementById('app');
+
+//This says we wanna render that template in that element. 
+ReactDOM.render(template, appRoot);
