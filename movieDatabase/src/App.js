0a03e8d82fc9fc53342df3e5cc15ea6e0388a534
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-02 20:17:57 -0500

    ok lets see what this does, this is the commit of another react project. I think I finally understand what it means and how it all works. Still have no idea why the HTML file didnt wanna commit on the first one.

---
+++ movieDatabase/src/App.js
@@ -0,0 +1,31 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+import moviesList from './moviesList';
+import movieDetail from './moviesDetail.js';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={moviesList} />
+        <Route path="/:id" component={movieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
