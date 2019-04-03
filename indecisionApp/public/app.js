822e1fb0387de59a925bc1bf73fd03aae80bfb7f
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 17:11:37 -0500

    going to install babel, so that older browsers can and will be able to understand the javascript.

--- indecisionApp/public/app.js
+++ indecisionApp/public/app.js
@@ -3,7 +3,11 @@ console.log('App.JS is running!')
 
 //JSX - Javascript XML(its basically just a language extension)
 
-const template = <p>A REACT THING</p>
+// const template = <p>A REACT THING</p>
+var template = React.createElement("p", null, "A REACT THING");
 const appRoot = document.getElementById('app');
 
+
+//This says we wanna render that template in that element. 
 ReactDOM.render(template, appRoot);
+
