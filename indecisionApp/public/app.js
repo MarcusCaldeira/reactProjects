b87538a09a712c5d3bedc5fc25cecfb1eeb04552
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 16:46:01 -0500

    unexpted token!

---
+++ indecisionApp/public/app.js
@@ -0,0 +1,9 @@
+console.log('App.JS is running!')
+
+
+//JSX - Javascript XML(its basically just a language extension)
+
+const template = <p>A REACT THING</p>
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
