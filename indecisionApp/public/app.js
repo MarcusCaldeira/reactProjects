6f28d3e45cdf03bfa7bf424095e31be9ffe3358f
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-04 11:53:51 -0500

    this is the prettier way to use JSX.. so that the contents are not on one line.

--- indecisionApp/public/app.js
+++ indecisionApp/public/app.js
@@ -7,20 +7,22 @@ console.log('App.JS is running!');
 
 //JSX - Javascript XML(its basically just a language extension)
 
+// In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'IDk Where one of those elements is coming from?'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'SO why do you need to be wrapped.'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'IDk Where one of those elements is coming from?'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'SO why do you need to be wrapped.'
+    )
 );
+
 var appRoot = document.getElementById('app');
 
 //This says we wanna render that template in that element. 
--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -6,7 +6,13 @@ console.log('App.JS is running!')
 
 //JSX - Javascript XML(its basically just a language extension)
 
-const template = <div><h1>IDk Where one of those elements is coming from?</h1><p>SO why do you need to be wrapped.</p></div>
+// In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
+const template =<div>
+    <h1>IDk Where one of those elements is coming from?</h1>    
+        <p>SO why do you need to be wrapped.</p>
+    </div>
+
+
 const appRoot = document.getElementById('app')
 
 
