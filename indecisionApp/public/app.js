41e48cca31622e64bede33fe5bafda481f5da1a5
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-09 14:11:09 -0500

    with const, you cant actually reassign it.

--- indecisionApp/public/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var varName = 'Marcus';
-var varName = 'Mike';
-console.log('varName: ' + varName);
-
-var nameLet = 'Marcus';
-nameLet = 'JUlie';
-console.log(nameLet);
--- indecisionApp/src/playground/es6LetConst.js
+++ indecisionApp/src/playground/es6LetConst.js
@@ -1,7 +1,11 @@
-var varName = 'Marcus'
-var varName = 'Mike'
-console.log('varName: ' + varName)
+// var varName = 'Marcus'
+// var varName = 'Mike'
+// console.log('varName: ' + varName)
 
-let nameLet = 'Marcus'
-nameLet = 'JUlie'
-console.log(nameLet)
+// let nameLet = 'Marcus'
+// nameLet = 'JUlie'
+// console.log(nameLet)
+
+const nameConst = 'Boogus'
+nameConst = 'Gunther'
+console.log(nameConst)
