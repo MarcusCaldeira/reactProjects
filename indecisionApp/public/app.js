da669f1314badecc4a983cfa1c2ee2e09abcea68
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-10 18:05:31 -0500

    ok i broke this so on the next commit I plan to push the solution.

--- indecisionApp/public/app.js
+++ indecisionApp/public/app.js
@@ -7,8 +7,12 @@ console.log('App.JS is running!');
 
 //JSX - Javascript XML(its basically just a language extension)
 
-// In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
-var templateOne = React.createElement(
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your hands in the life of a computer',
+    options: ['one', 'two']
+    // In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
+};var templateOne = React.createElement(
     'div',
     null,
     React.createElement(
@@ -36,49 +40,5 @@ var templateOne = React.createElement(
         )
     )
 );
-
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    console.log('Add One', count);
-};
-var minusOne = function minusOne() {
-    console.log('Minus One');
-};
-var reset = function reset() {
-    console.log('Reset');
-};
-
 var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Count: ',
-            count
-        ),
-        React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'Reset'
-        )
-    );
-    ReactDOM.render(templateTwo, appRoot);
-    //This says we wanna render that template in that element. 
-    // ReactDOM.render(templateTwo, appRoot)
-};
-renderCounterApp();
+ReactDOM.render(templateOne, app);
--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -6,6 +6,11 @@ console.log('App.JS is running!')
 
 //JSX - Javascript XML(its basically just a language extension)
 
+const app = {
+    title:'Indecision App',
+    subtitle:'Put your hands in the life of a computer',
+    options:['one','two']
+}
 // In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
 const templateOne = (
 <div>
@@ -17,39 +22,5 @@ const templateOne = (
         </ol>
  </div>
  )
-
-
-
-
-
-
-
- let count = 0 
- const addOne = () =>{
-     count++
-     console.log('Add One', count)
- }
- const minusOne = () => {
-     console.log('Minus One')
- }
- const reset = () => {
-     console.log('Reset')
- }
-
 const appRoot = document.getElementById('app')
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick = {addOne}>+1</button> 
-            <button onClick = {minusOne}>-1</button> 
-            <button onClick = {reset}>Reset</button> 
-        </div>
-    )
-    ReactDOM.render(templateTwo, appRoot)
-    //This says we wanna render that template in that element. 
-    // ReactDOM.render(templateTwo, appRoot)
-}
-renderCounterApp()
-
+ReactDOM.render(templateOne, app)
