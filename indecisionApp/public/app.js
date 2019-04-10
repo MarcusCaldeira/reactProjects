63b29060456062fa2580932a6ae014a93947ed23
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-10 18:45:14 -0500

    need to add dynamic options text, but stopping here for the night

--- indecisionApp/public/app.js
+++ indecisionApp/public/app.js
@@ -10,40 +10,68 @@ console.log('App.JS is running!');
 var app = {
     title: 'Indecision App',
     subtitle: 'Put your hands in the life of a computer',
-    options: ['one', 'two']
-    // In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
-};var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Changed this a bit to early'
-    ),
-    React.createElement(
-        'p',
-        null,
-        'SO why do you need to be wrapped.'
-    ),
-    React.createElement(
-        'ol',
+    options: []
+};
+var onFormSubmit = function onFormSubmit(e) {
+    //This will stop the browser window from reloading
+    e.preventDefault();
+    //Points to the element that the event started on. 
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+// In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
+
+
+var appRoot = document.getElementById('app');
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
         null,
         React.createElement(
-            'li',
+            'h1',
             null,
-            'IS this gonna work?'
+            'Changed this a bit to early'
         ),
         React.createElement(
-            'li',
+            'p',
             null,
-            'So that one worked? why did this one break?'
+            'SO why do you need to be wrapped.'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'IS this gonna work?'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'So that one worked? why did this one break?'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
         )
-    ),
-    React.createElement(
-        'form',
-        null,
-        React.createElement('input', { type: 'text', name: 'option' })
-    )
-);
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+    );
+    ReactDOM.render(template, appRoot);
+};
+render();
--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -9,21 +9,43 @@ console.log('App.JS is running!')
 const app = {
     title:'Indecision App',
     subtitle:'Put your hands in the life of a computer',
-    options:['one','two']
+    options:[]
+}
+const onFormSubmit = (e) =>{
+    //This will stop the browser window from reloading
+    e.preventDefault();
+    //Points to the element that the event started on. 
+    const option = e.target.elements.option.value
+    if (option){
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        render()   
+    }
+
 }
 // In JSX you can have many JSX Expressions they just have to be wrapped in a single root element. 
-const template = (
-<div>
-    <h1>Changed this a bit to early</h1>    
-        <p>SO why do you need to be wrapped.</p>
-        <ol>
-            <li>IS this gonna work?</li>
-            <li>So that one worked? why did this one break?</li>
-        </ol>
-        <form>
-        <input type ="text" name="option"></input>
-        </form>
- </div>
- )
+
+
+
 const appRoot = document.getElementById('app')
+
+
+const render = () =>{
+    const template = (
+        <div>
+            <h1>Changed this a bit to early</h1>    
+                <p>SO why do you need to be wrapped.</p>
+                <p>{app.options.length}</p>
+                <ol>
+                    <li>IS this gonna work?</li>
+                    <li>So that one worked? why did this one break?</li>
+                </ol>
+                <form onSubmit={onFormSubmit}>
+                <input type ="text" name="option"></input>
+                <button>Add Option</button>
+                </form>
+         </div>
+         )
 ReactDOM.render(template, appRoot)
+}
+render()
