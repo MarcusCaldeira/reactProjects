bec9cb46436eae3b4076569fd6b47dc4359df835
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-16 17:25:59 -0500

    calling the thing you made with the header, that I placed there.

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -1,1 +1,16 @@
 console.log('App is running')
+
+class Header extends React.Component {
+    render(){
+        return <p>This is from Header</p>
+    }
+
+}
+const jsx = (
+    <div>
+        <h1>TItle</h1>
+        <Header/>
+    </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
