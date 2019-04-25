5e70d5eeb07b3f0e249ae235daafbe7c9b477464
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-25 14:18:43 -0500

    there is bundle.js it looks much like babel

--- indecisionApp/webpack.config.js
+++ indecisionApp/webpack.config.js
@@ -1,12 +1,13 @@
+const path = require('path')
 //Entry 
 
 //How to find the absolute path by using Node. 
-console.log(__dirname)
+// console.log(path.join(__dirname, 'public'))
 
 module.exports = {
     entry: './src/app.js',
     output:{
-        path: '',
+        path: path.join(__dirname, 'public'),
         filename:'bundle.js'
     }
 
