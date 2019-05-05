78a4e2acd533c23f40a90dc14e817e5c56ec039f
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-05 16:31:27 -0500

    I accidently deleted the app.js file and had to get it from github. Now it has an error im gonna try to fix that

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const template = <p>This is JSX From Webpack</p>
+const template = React.createElement('p', {}, 'Testing123')
 
 
 ReactDOM.render(template, document.getElementById('app'))
