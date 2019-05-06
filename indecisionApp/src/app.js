6749875a0278281edaccae016df7a1b1c34c9aa4
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-06 18:05:40 -0500

    new syntax vs the old syntax

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -2,10 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 
-
-// stateless functional component
-
-  
   
   ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+
+
+  class OldSyntax {
+      constructor(){
+          this.name='Mike'
+      }
+  }
+  const oldSyntax = new OldSyntax()
+  console.log(oldSyntax)
+
+
+  //---- 
+class NewSyntax{
+    name = 'Jen'
+
+}
+const newSyntax = new NewSyntax()
+console.log(newSyntax)
   
