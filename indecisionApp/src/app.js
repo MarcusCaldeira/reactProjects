d18eb7959c0d5048eeb5e4b70c94968637375281
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-09 14:47:00 -0500

    going to create a layout for the app itsself, where we want a heder and a layout page.

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -2,34 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 
-  
-  ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
-
-
-  class OldSyntax {
-      constructor(){
-          this.name='Mike'
-          this.getGreeting = this.getGreeting.bind(this)
-      }
-      getGreeting(){
-          return `hi my name is ${this.name}`
-      }
-
-  }
-  const oldSyntax = new OldSyntax()
-  const getGreeting = oldSyntax.getGreeting
-  console.log(getGreeting())
-
-
-  //---- 
-class NewSyntax{
-    name = 'Jen'
-    getGreeting = () =>{
-        return `hi my name is ${this.name}`
-    }
-
+const layout = () => {
+    return
+    <div>
+    
+    </div>
 }
-const newSyntax = new NewSyntax()
-const newGetGreeting = newSyntax.getGreeting
-console.log(newGetGreeting())
+
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
   
