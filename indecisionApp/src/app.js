2ffbc3cf5fbce4b5fe0d8b92fb5265f23d83668b
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-09 15:12:11 -0500

    the app is up and running, Im curious how this all runs together.

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -2,24 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 
-const Layout = (props) => {
-    return(
-    <div>
-        <p>Header</p>
-        {props.children}
-        <p>Footer</p>
-    </div>
-    )
-}
-const template = (
-    <div>
-        <h1>Page Title</h1>
-        <p>Actual page content</p>
-    </div>
-)
 
 
 
 
-ReactDOM.render(<Layout><p>THIS IS INLINE</p></Layout>, document.getElementById('app'))
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
   
